5839507c48def065619e4efb76a990a531aad037
itskegnh <itskegnh@gmail.com>
2024-07-11 14:12:02 +1000

    change resend to the fetch API

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -1,20 +1,27 @@
 // app/api/contact/route.js
 import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
 
 export const POST = async (req) => {
     const { name, email, subject, message } = await req.json();
 
-    const resend = new Resend(process.env.RESEND_API_KEY);
-
-    const { data, error } = resend.emails.send({
-        from: 'noreply <noreply@williamjackson.dev>',
-        to: process.env.CONTACT_EMAIL,
-        subject: 'Form Submission: ' + subject,
-        html: `${name} (${email}) has sent you a message:<br><br>${message}`
+    const res = await fetch('https://api.resend.com/emails', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.RESEND_API_KEY}`,
+        },
+        body: JSON.stringify({
+            from: 'noreply <noreply@williamjackson.dev>',
+            to: process.env.CONTACT_EMAIL,
+            subject: 'Form Submission: ' + subject,
+            html: `${name} (${email}) has sent you a message:<br><br>${message}`
+        }),
     });
 
-    console.log(data, error);
+    if (res.ok) {
+        const data = await res.json();
+        return Response.json(data);
+    }
 
     return NextResponse.json({ success: true });
 };
