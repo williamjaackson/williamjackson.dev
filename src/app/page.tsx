f8dada53cf5f884ca839aec8ec7fe53b10cefaa9
itskegnh <itskegnh@gmail.com>
2024-07-11 12:13:02 +1000

    finished homepage frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,103 @@
+import { Abril_Fatface } from "next/font/google";
 import Image from "next/image";
 
+const abril_fatface = Abril_Fatface({ weight: "400", preload: false });
+
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    return (
+        <>
+        <section className="flex items-center justify-center h-screen p-8 bg-primary shadow-lg z-10 relative overflow-hidden">
+            <div className="grid md:flex gap-8 z-30 place-items-center">
+                <Image
+                    src="/images/logo-full-transparent.png"
+                    alt="logo"
+                    width={1024}
+                    height={1024}
+                    className="size-32 md:size-60"
+                />
+                <div className="text-center md:text-left py-auto">
+                    <p className="text-xl md:text-3xl text-yellow-700 text-left">Hello World! I'm,</p>
+                    <h1 className={`text-3xl md:text-5xl text-yellow-950 font-bold font-title ${abril_fatface.className}`}>William Jackson</h1>
+                    <div className="mt-2 flex gap-2 flex-col md:flex-row">
+                        <a href="#about" className="bg-primary px-6 py-1 border-2 border-yellow-800 text-yellow-900 rounded-md hover:bg-yellow-800 hover:text-primary md:text-xl"
+                            >About Me</a>
+                        <a href="#projects" className="bg-primary px-6 py-1 border-2 border-yellow-800 text-yellow-900 rounded-md hover:bg-yellow-800 hover:text-primary md:text-xl"
+                            >Projects</a>
+                        <a href="#contact" className="bg-primary px-6 py-1 border-2 border-yellow-800 text-yellow-900 rounded-md hover:bg-yellow-800 hover:text-primary md:text-xl"
+                            >Contact</a>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <img src="/images/hexagon.png" alt="hexagon" className="absolute top-[-6rem] left-[-2rem] w-48 h-48 z-20" />
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+                <img src="/images/hexagon.png" alt="hexagon" className="absolute bottom-[-1rem] right-[-3rem] w-32 h-32 z-20" />
+                <img src="/images/hexagon.png" alt="hexagon" className="absolute bottom-[7rem] right-[-1rem] w-32 h-32 z-20" />
+                <img src="/images/hexagon.png" alt="hexagon" className="absolute bottom-[1rem] right-[5rem] w-32 h-32 z-20" />
+            </div>
+        </section>
+        <section id="about" className="bg-white p-8 md:pt-16 md:px-32">
+            <div className="grid max-w-3xl mx-auto">
+                <h2 className={`text-3xl md:text-5xl text-yellow-900 font-bold font-title ${abril_fatface.className}`}>
+                    <span className="hidden md:inline">A Little </span>
+                    About Me</h2>
+                <p className="border-l-2 border-yellow-800 pl-6 md:text-xl text-slate-800 mt-4">I am currently a high school student with a passion for technology and programming. For nearly a decade I have been programming as a hobby, and during this time I've learnt a lot through experience. I am constantly on the lookout for new projects to take on and new skills to acquire. My love for problem-solving drives me to continually seek out and overcome new challenges.</p>
+                <span className="text-right md:text-xl text-slate-800 mt-4">- William Jackson</span>
+            </div>
+        </section>
+        <section id="projects" className="bg-gray-100 p-8 md:pt-16 md:px-32">
+            <div className="grid max-w-3xl mx-auto">
+                <h2 className={`text-3xl md:text-5xl text-yellow-900 font-bold font-title ${abril_fatface.className}`}>
+                    My Projects</h2>
+                {/* say projects will be added here as completed */}
+                <p className="md:text-xl text-slate-500 m-8 bg-gray-200 p-4 rounded-md px-8 mx-auto">Projects will be added here as they are completed.</p>
+            </div>
+        </section>
+        {/* contact form */}
+        <section id="contact" className="bg-white p-8 md:pt-16 md:px-32">
+            <div className="grid max-w-3xl mx-auto">
+                <h2 className={`text-3xl md:text-5xl text-yellow-900 font-bold font-title ${abril_fatface.className}`}>
+                    <span className="hidden md:inline">Send me an </span>
+                    Email
+                    <span className="inline md:hidden"> me</span>
+                </h2>
+                <p className="md:text-xl text-slate-800 mt-4">Have a question or want to get in touch? Fill out the form below and I'll get back to you as soon as possible.</p>
+                <form className="grid mt-4">
+                    <h3 className="text-2xl md:text-3xl text-yellow-900 font-bold">Contact Details</h3>
+                    <div className="grid gap-2">
+                        <div className="grid">
+                            <label htmlFor="name" className="text-lg text-slate-500">Full Name
+                                <span className="text-red-500"> *</span>
+                            </label>
+                            <input type="text" id="name" className="border border-gray-300 p-2 rounded-md" placeholder="What should I call you?"/>
+                        </div>
+                        <div className="grid">
+                            <label htmlFor="email" className="text-lg text-slate-500">Email Address
+                                <span className="text-red-500"> *</span>
+                            </label>
+                            <input type="email" id="email" className="border border-gray-300 p-2 rounded-md" placeholder="What email address can I reach you at?"/>
+                        </div>
+                    </div>
+                    <h3 className="text-2xl md:text-3xl mt-4 text-yellow-900 font-bold">Message</h3>
+                    <div className="grid gap-2">
+                        <div className="grid">
+                            <label htmlFor="message" className="text-lg text-slate-500">Subject
+                                <span className="text-red-500"> *</span>
+                            </label>
+                            <input type="text" id="subject" className="border border-gray-300 p-2 rounded-md" placeholder="What is your message about?"/>
+                        </div>
+                        <div className="grid">
+                            <label htmlFor="message" className="text-lg text-slate-500">Content
+                                <span className="text-red-500"> *</span>
+                            </label>
+                            <textarea id="message" className="border border-gray-300 p-2 rounded-md h-32" placeholder="What would you like to say?"/>
+                        </div>
+                    </div>
+                    <button className="w-fit px-8 bg-primary text-yellow-900 hover:text-white text-xl py-2 rounded-md mt-4 hover:bg-yellow-700">Send Email</button>
+                </form>
+            </div>
+        </section>
+        {/* footer */}
+        </>
+    );
 }
