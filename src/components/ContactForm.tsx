502c097a4c0f9c1da5555c26df898654735d4b99
itskegnh <itskegnh@gmail.com>
2024-07-11 14:32:11 +1000

    Loading and Success/Error states for contact form

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -10,6 +10,11 @@ export const ContactForm = () => {
         message: "",
     });
 
+    const [error, setError] = useState(false);
+    const [success, setSuccess] = useState(false);
+
+    const [loading, setLoading] = useState(false);
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { id, value } = e.target;
         setFormData((prev) => ({
@@ -20,6 +25,9 @@ export const ContactForm = () => {
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+        setError(false);
+        setSuccess(false);
+        setLoading(true);
         try {
             const response = await fetch('/api/contact', {
                 method: 'POST',
@@ -31,20 +39,33 @@ export const ContactForm = () => {
         
             if (response.ok) {
                 // Handle success
-                alert('Message sent successfully!');
+                setSuccess(true);
                 setFormData({ name: '', email: '', subject:'', message: '' });
             } else {
                 // Handle error
-                alert('Failed to send message. Please try again.');
+                setError(true);
             }
         } catch (error) {
             console.error('Error submitting form:', error);
-            alert('An error occurred. Please try again.');
+            setError(true);
         }
+        setLoading(false);
     };
 
     return (
         <form className="grid mt-4" onSubmit={handleSubmit}>
+            {
+                error && <div className={`bg-red-300 border border-red-500 text-red-600 p-4 rounded-md grid md:flex gap-2 mb-4`}>
+                    <span className="text-xl font-bold">Something went wrong!</span>
+                    <span className="text-lg">Your message was not delivered.</span>
+                </div>
+            }
+            {
+                success && <div className="bg-gray-300 border border-gray-500 text-gray-600 p-4 rounded-md grid md:flex gap-2 mb-4">
+                    <span className="text-xl font-bold">Success!</span>
+                    <span className="text-lg">Your message was delivered.</span>
+                </div>
+            }
             <h3 className="text-2xl md:text-3xl text-yellow-900 font-bold">Contact Details</h3>
             <div className="grid gap-2">
                 <div className="grid">
@@ -75,7 +96,13 @@ export const ContactForm = () => {
                     <textarea id="message" className="border border-gray-300 p-2 rounded-md h-32" placeholder="What would you like to say?" value={formData.message} onChange={handleChange} required/>
                 </div>
             </div>
-            <button className="w-fit px-8 bg-primary text-yellow-900 hover:text-white text-xl py-2 rounded-md mt-4 hover:bg-yellow-700">Send Email</button>
+            <button className="w-fit px-8 bg-primary text-yellow-900 hover:text-white text-xl py-2 rounded-md mt-4 hover:bg-yellow-700 disabled:bg-gray-300 disabled:text-gray-600 disabled:cursor-not-allowed" disabled={loading}>
+            {
+                loading 
+                ? <div className="my-2 rounded-full w-6 h-6 border-4 border-gray-600 border-t-white animate-spin"></div>
+                : "Send Email"
+            }
+            </button>
         </form>
     );
 }
