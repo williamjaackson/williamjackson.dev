914e13755340fc58c5402cf1d01080f4658877a2
William Jackson <itskegnh@gmail.com>
2024-09-01 17:07:45 +1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,20 +37,10 @@ export default function Home() {
                 <Image src="/images/hexagon.png" alt="hexagon" width={256} height={256} className="absolute bottom-[1rem] right-[5rem] w-32 h-32 z-20" />
             </div>
         </section>
-        <section id="about" className="bg-white p-8 md:pt-16 md:px-32">
-            <div className="grid max-w-3xl mx-auto">
-                <h2 className={`text-3xl md:text-5xl text-yellow-900 font-bold font-title ${abril_fatface.className}`}>
-                    <span className="hidden md:inline">A Little </span>
-                    About Me</h2>
-                <p className="border-l-2 border-yellow-800 pl-6 md:text-xl text-slate-800 mt-4">I am currently a high school student with a passion for technology and programming. For nearly a decade I have been programming as a hobby, and during this time I&apos;ve learnt a lot through experience. I am constantly on the lookout for new projects to take on and new skills to acquire. My love for problem-solving drives me to continually seek out and overcome new challenges.</p>
-                <span className="text-right md:text-xl text-slate-800 mt-4">- William Jackson</span>
-            </div>
-        </section>
         <section id="projects" className="bg-gray-100 p-8 md:pt-16 md:px-32">
             <div className="grid max-w-3xl mx-auto">
                 <h2 className={`text-3xl md:text-5xl text-yellow-900 font-bold font-title ${abril_fatface.className}`}>
                     My Projects</h2>
-                {/* say projects will be added here as completed */}
                 <p className="md:text-xl text-slate-500 m-8 bg-gray-200 p-4 rounded-md px-8 mx-auto">Projects will be added here as they are completed.</p>
             </div>
         </section>
